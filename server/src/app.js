b2a8d7fa7f09b375c57a4af0a0368dd6adfe5c6f
minjman2659 <minjman2659@gmail.com>
2021-12-13 16:27:16 +0900

    [Server] cors added

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 const { consumeToken, errorHandler, missingPath } = require('middleware');
@@ -9,8 +10,15 @@ const routes = require('./routes');
 // express를 이용한 서버 구현
 const app = express();
 
-app.use(cookieParser());
 app.use(express.json({ limit: '30mb' }));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+
 app.use('/images', express.static(imagesDir));
 
 app.use(consumeToken);
