b3ca0183d5760c21a927395fc0c70e5310c318ae
minjman2659 <minjman2659@gmail.com>
2021-12-17 23:02:52 +0900

    [Server] Movie response modified

--- server/src/lib/movies-dir.js
+++ server/src/lib/movies-dir.js
@@ -11,32 +11,23 @@ router.get('*', (req, res) => {
   const { range } = req.headers;
   console.log(range);
 
-  if (!range) {
-    const header = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    };
-    res.writeHead(200, header);
-    res.end();
-  } else {
-    const MAX_CHUNK_SIZE = 1000 * 1000 * 50;
-    // range 헤더 파싱
-    const parts = range.replace(/bytes=/, '').split('-');
-    // 재생 구간 설정
-    const start = parseInt(parts[0], 10);
-    const _end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-    const end = Math.min(_end, start + MAX_CHUNK_SIZE - 1);
+  const MAX_CHUNK_SIZE = 1000 * 1000 * 50;
+  // range 헤더 파싱
+  const parts = range.replace(/bytes=/, '').split('-');
+  // 재생 구간 설정
+  const start = parseInt(parts[0], 10);
+  const _end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+  const end = Math.min(_end, start + MAX_CHUNK_SIZE - 1);
 
-    const header = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': `bytes`,
-      'Content-Type': 'video/mp4',
-      'Content-Length': fileSize - 1,
-    };
-    res.writeHead(206, header);
-    const readStream = fs.createReadStream(filePath, { start, end });
-    readStream.pipe(res);
-  }
+  const header = {
+    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+    'Accept-Ranges': `bytes`,
+    'Content-Type': 'video/mp4',
+    'Content-Length': end - start + 1,
+  };
+  res.writeHead(206, header);
+  const readStream = fs.createReadStream(filePath, { start, end });
+  readStream.pipe(res);
 });
 
 module.exports = router;
