a3f0ae556839ba960a5b9772a775bda764b00743
minjman2659 <minjman2659@gmail.com>
2021-12-21 17:27:37 +0900

    [Server] Video modified

--- server/src/routes/videos-dir.js
+++ server/src/routes/videos-dir.js
@@ -12,13 +12,14 @@ router.get('*', (req, res) => {
   const { range } = req.headers;
   console.log(range);
 
-  const MAX_CHUNK_SIZE = 1000 * 1000 * 50;
+  const MAX_CHUNK_SIZE = 1000 * 1000;
   // range 헤더 파싱
   const parts = range.replace(/bytes=/, '').split('-');
   // 재생 구간 설정
   const start = parseInt(parts[0], 10);
   const _end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
   const end = Math.min(_end, start + MAX_CHUNK_SIZE - 1);
+  // console.log(start, _end, end);
 
   const header = {
     'Content-Range': `bytes ${start}-${end}/${fileSize}`,
